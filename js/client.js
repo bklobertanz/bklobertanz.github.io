c936e6d7417d10a5dc4c251ee9a7b3d97599eb70
bklobertanz <klobertanz@gmail.com>
2020-03-10 05:00:58 -0300

    Interaccion con horario semanal añadida y mensaje de resumen dinamico.

--- js/client.js
+++ js/client.js
@@ -1,10 +1,9 @@
 'use strict';
 
-window.addEventListener('load',()=>{
-    
-    //Compartamiento de los botones de los modals
+$(document).ready(()=>{
+     //Compartamiento de los botones de los modals
 
-    $('.btnConoceme').on('click',()=>{
+     $('.btnConoceme').on('click',()=>{
         $('#modalConoceme').modal('show')
     });
 
@@ -17,13 +16,78 @@ window.addEventListener('load',()=>{
     $('.btnConfirmarVisita').on('click',()=>{
         $('#modalSolVisita').modal('hide');
         $('#modalConfirmarVisita').modal('show');
-        
+        precioTotal();
+        escribirModalResumen(localStorage.getItem('horasTotales'),localStorage.getItem('precioTotal'));
     });
 
-    //Tabla
+    $('.btnDismiss').on('click',()=>{
+        $('html, body').scrollTop(0);
+        location.reload();
+    })
 
-    $('table tbody tr td').on('click',()=>{
-       // identificar sobre cual td se hizo click
+    //Selección de horarios de cuidado
+    let horarioTrabajo = []
+    const precioCuidador = 3000;
+
+    $('table tbody td').on('click',(ev)=>{
+
+        // Identificando horario (hora y día de la semana) seleccionado
+        const semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+        const td = ev.target;
+        const tdIndex = td.cellIndex;
+        const tr = td.parentNode;
+        const th = tr.cells[0];
+        const thText = th.firstChild.data;
+        const horario = `${semana[tdIndex-1]} - ${thText}`;
+
+        // Cambiar la celda de disponible a seleccionado
+            if(!$(td).hasClass('ocupado'))
+            {   
+                $(td).addClass('ocupado');
+                td.childNodes[0].data = 'Seleccionado';
+                horarioTrabajo.push(horario);
+            }
+            else{
+                $(td).removeClass('ocupado');
+                td.childNodes[0].data = 'Disponible';
+                popByIndex(horario,horarioTrabajo);
+            }
+            actualizarHorasPrecio(horarioTrabajo.length, precioCuidador*horarioTrabajo.length); 
     });
+
+    
+    function actualizarHorasPrecio(horas,precio){
+        $('#horasSeleccionadas').text(`Horas seleccionadas para cuidado: ${horas}.`);
+
+        $('#totalSeleccionado').text(`Total: $${precio}.`)
+    }
+    //Determinar precio total
+    function precioTotal(){
+        const horas = horarioTrabajo.length;
+        localStorage.setItem('precioTotal', horas * precioCuidador);
+        localStorage.setItem('horasTotales',horas);
+    }
+
+    function escribirModalResumen(horasTotales,precioTotal){
+
+        let p1 = '<p>Nombre cuidador realizará los siguientes cuidados:</p>';
+        var lista = '<ul>';
+        horarioTrabajo.forEach(horario =>{
+            lista+=`<li>${horario}</li>`;
+        });
+        lista+='</ul>';
+        let p2 =`<p>Esto sería un total de $${precioTotal}, ya que nuestro cuidador trabajará un total de ${horasTotales} hora(s).<p>`;
+        let p3 = '<p>El cuidador se presentará en el domicilio que usted especificó.</p>';
+        let p4 = '<p>Gracias nombre usuario por confiar en nosotros!</p>';
+        $('#modalConfirmarVisita .modal-body').append(p1,lista,p2,p3,p4);
+    }
+    function popByIndex(element,array){
+        let index = array.indexOf(element);
+        array.splice(index,1);
+    }
 });
+    
+   
+   
 
+ 
